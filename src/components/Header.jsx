310dba47683d2bbdffddc932878a4332873628c1
Akash Srivastava <akash221002@gmail.com>
2022-10-08 21:08:00 +0530

    Designed Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BiMoviePlay } from "react-icons/bi";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between py-4 px-2">
+      <div className="flex items-center space-x-2">
+        <h1 className="text-white font-bold text-2xl">Movies</h1>
+        <BiMoviePlay className="block sm:visible sm:text-white sm:text-3xl sm:font-bold" />
+      </div>
+      <input
+        type="search"
+        placeholder="Search..."
+        className="w-60 px-2 py-2 sm:px-4 sm:mr-2 xl:px-4 xl:w-96 xl:py-2 rounded-md"
+      />
+    </div>
+  );
+};
+
+export default Header;
