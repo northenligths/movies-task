b7846663f556e448c8ee36a3f368a8e5518b4709
Akash Srivastava <akash221002@gmail.com>
2022-10-08 20:43:56 +0530

    Started Project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
